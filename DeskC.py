20a56b9c68b3bf83cf416bfc81fe5e0b085e56df
Chase Cantrell <66339719+audyviii@users.noreply.github.com>
2022-01-28 18:41:36 -0600

    Revised PATH & Duplicate Error Handling
    For readability: 
    - I updated some of the PATH names to better assist with understanding where substitutions are for testing purposes.
    - Created variable for clarification.
    - Added comments for clarification.
    
    
    For Duplicate Error Handling:
    - For now, I have created an additional folder to send the duplicates to for manual renaming-- until revised otherwise.
    - Changed the output of the 'FileNotFoundError' so that it is a better prompt, to the developer, in the situation that an extension is not predefined within the folder structure.

--- DeskC.py
+++ DeskC.py
@@ -6,12 +6,14 @@ from collections import Counter
 import tkinter as tk
 from tkinter import ttk
 
-# Windows DPI adjustment
+# Windows DPI adjustment for GUI 
 try:
     from ctypes import windll
     windll.shcore.SetProcessDpiAwareness(1)
 except:
     pass
+
+FULL_PATH_TO_DESIGNATED_FOLDER = 'You put full path of folder you want to file(s) to.'
 
                   # Word Processing
 file_extensions = {'.txt' : 'FULL_PATH_TO_DESIGNATED_FOLDER',
@@ -25,9 +27,9 @@ def scan_files(main_dir):
         global files, extensions, full_file_names
         files = os.listdir(main_dir) 
         if 'desktop.ini' in files: 
-                files.remove('desktop.ini')  
+                files.remove('desktop.ini')  # Prevents .ini file deletion
         elif 'downloads.ini' in files:
-                files.remove('downloads.ini')
+                files.remove('downloads.ini') # Prevents .ini file deletion
         else:
                 pass
         extensions = [] 
@@ -50,28 +52,28 @@ def organize_files():
                         dest_folder.append(a)
         def file_handle():                
                 for k in full_file_names: 
-                        criteria = os.path.splitext(k)[1]
+                        criteria = os.path.splitext(k)[1] # takes item in list, stores variable with extension split from full path name
                         try:
                                 if criteria in file_extensions:
                                         path_to = file_extensions.get(criteria) 
-                                        shutil.move(k, path_to) # moves file to location based on 'criteria'
+                                        shutil.move(k, path_to) # moves file to location based on 'criteria' (criteria = extension keys in dictionary)
                         except shutil.Error:
-                                print(f'Rename "{k}". That file name already exists. \n')
+                                shutil.move(k, "PATH_TO_DUPLICATES FOLDER")
                         except FileNotFoundError:
-                                print('Directory Not Defined! \n')
+                                print('You need to add this extension pre-defined dictionary structure. \n')
                                 print(criteria)
 
         file_handle()
 
 
 def clean_downloads():
-        scan_files('C:\\Users\\Admin\\Downloads\\')
+        scan_files('C:\\Users\\User\\Type_USER_Here\\Admin\\Downloads\\')
         organize_files()
         root.destroy()
 
 
 def clean_desktop():
-        scan_files('C:\\Users\\Admin\\Desktop\\')
+        scan_files('C:\\Users\\Type_USER_Here\\Admin\\Desktop\\')
         organize_files()
         root.destroy()
 
